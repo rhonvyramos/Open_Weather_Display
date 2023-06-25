ef516ded8ff2e45962ddfebb3b244279bd2e5aff
rhonvyramos <rhonvyramos@gmail.com>
2023-06-25 17:15:30 -0400

    created functionality to change main display city name to name of searched city

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 // this key defaults to a hidden file not pushed into GitHub
 // please provide your own Open Weather API key to use the website fully
 import { api_key_openweather } from "../keys/api_key_open_weather.js";
-let api_key = "";
+let api_key = api_key_openweather;
 
 // this function always executes first to ensure that the api key variable is present
 // if it is empty, simply discontinue the use of the webpage
@@ -11,8 +11,14 @@ $(function () {
     };
 });
 
+/*
+ * main website functionality body
+ */
+
 let city_search_input = document.getElementById("city_search_input");
 let city_search_button = document.getElementById("city_search_button");
+
+let display_main_title = document.getElementById("display_main_title");
 
 city_search_button.addEventListener("click", search_city);
 
@@ -24,9 +30,22 @@ async function search_city() {
 
     let london_data = await fetch("https://api.openweathermap.org/geo/1.0/direct?q=" 
                     + search_input 
-                    + "&limit=5&appid={key_here}");
+                    + "&limit=5&appid=" + api_key);
     
-    let json_city_data = await london_data.json();
+    let json_city_location_data = await london_data.json();
 
-    console.log(json_city_data);
+    let city_name = json_city_location_data[0]["name"];
+    let city_lat = json_city_location_data[0]["lat"];
+    let city_lon = json_city_location_data[0]["lon"];
+    
+    let city_weather_data = await fetch("https://api.openweathermap.org/data/2.5/weather?lat="
+                          + city_lat 
+                          + "&lon=" 
+                          + city_lon 
+                          + "&appid=" 
+                          + api_key);
+    
+    let json_city_weather_data = await city_weather_data.json();
+
+    display_main_title.innerHTML = json_city_weather_data.name;
 };
